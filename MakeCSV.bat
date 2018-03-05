9c0794fa1c60367630ffa9920be0f4d05faa4617
chrisstring <35812821+chrisstring@users.noreply.github.com>
2018-03-05 12:34:23 -0500

    Production commit

--- MakeCSV.bat
+++ MakeCSV.bat
@@ -1,7 +1,8 @@
 @echo off
 cls
 echo "This will EXCLUDE all files with a CSV file extension"
-set /p filePath="Please PASTE the path to the folder that you want [shift] + [INS]:"
+set /p filePath="Please PASTE the path to the folder that you want using [shift] + [INS]:"
 pushd %filePath%
 dir /b | findstr /v /i "\.csv$" > filelist.csv
 echo Operation successful, located at:%filePath%filelist.csv
+pause
