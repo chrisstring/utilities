c673cd1affe3b09e1e838af8f587ba9e05feefa1
chrisstring <35812821+chrisstring@users.noreply.github.com>
2018-03-09 11:33:51 -0500

    Replaces Make CSV.bat

--- ListAllFilesInDirectoryToCSV.bat
+++ ListAllFilesInDirectoryToCSV.bat
@@ -3,6 +3,6 @@ cls
 echo "This will EXCLUDE all files with a CSV file extension"
 set /p filePath="Please PASTE the path to the folder that you want using [shift] + [INS]:"
 pushd %filePath%
-dir /b | findstr /v /i "\.csv$" > filelist.csv
+for /r %%i in (*) do @echo %%~nxi >> filelist.csv
 echo Operation successful, located at:%filePath%filelist.csv
 pause
